a342b1b59c780ffb8dbbbd3d9fd724a58d16316d
anton <semchenkoanton@gmail.com>
2019-04-10 18:08:21 +0200

    (WIP) ResourceManager.cs

---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ExtraTools
+{
+    public class ResourceManager : Singleton<ResourceManager>
+    {
+        [Serializable]
+        private class ObjectHolder
+        {
+            [SerializeField]
+            private GameObject prefab;
+            [SerializeField]
+            private List<GameObject> instantiated = new List<GameObject>();
+        }
+    }
+}
